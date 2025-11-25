84264a03172a949badd75bd9536f784eafb19af0
michael.pitt <michael.pitt1@justice.gov.uk>
2025-11-25 11:14:19 +0000

    PIC-4958 - addressed TODOs

--- src/test/kotlin/uk/gov/justice/digital/hmpps/courtcasedatamigrationservice/processor/OffenceProcessorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/courtcasedatamigrationservice/processor/OffenceProcessorTest.kt
@@ -13,9 +13,6 @@ import uk.gov.justice.digital.hmpps.courtcasedatamigrationservice.domain.target.
 import uk.gov.justice.digital.hmpps.courtcasedatamigrationservice.domain.target.Verdict
 import java.math.BigDecimal
 import java.sql.Timestamp
-import java.time.Clock
-import java.time.Instant
-import java.time.LocalDateTime
 import java.time.OffsetDateTime
 import java.time.ZoneId
 
